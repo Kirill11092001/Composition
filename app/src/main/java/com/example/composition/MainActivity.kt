367af90c3f214f3b8fbdce91062c809f026df80b
mka23 <mka230704@yandex.ru>
2024-10-08 16:09:52 +0300

    start composition

--- app/src/main/java/com/example/composition/MainActivity.kt
+++ app/src/main/java/com/example/composition/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val a = 3
+
         val b = 5
         val c = 5
     }
